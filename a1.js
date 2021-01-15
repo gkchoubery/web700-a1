de4d9cf5c3a316f1433878abd6be2e4aa1c22436
Geet Choubey <gkchoubery@myseneca.ca>
2021-01-15 23:23:46 +0530

    Added package json and student details

--- a1.js
+++ a1.js
@@ -1,10 +1,10 @@
 /**********************************************************************************  
  * WEB700–Assignment1
  * I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
- *  No part of this assignment has been copied manually or electronically from any other source
+ * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students
  * 
- * Name: Geet Kumar Choubey Student ID: ______________ Date: ________________
+ * Name: Geet Kumar Choubey Student ID: 155876196 Date: 15-Jan-2021
  * 
  ********************************************************************************/
 
